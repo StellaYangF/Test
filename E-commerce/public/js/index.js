7088cacfda7566eee62bd0f6cdbe69bbeccce232
StellaYangF <1062878468@qq.com>
2019-03-02 17:56:11 +0800

    index.html navs

--- E-commerce/public/js/index.js
+++ E-commerce/public/js/index.js
@@ -23,24 +23,46 @@
         .css({ position: "relative" });
     // navs悬停设置
     var $ul = $("#header>ul");
-    $ul.on("mouseenter", "li", function () {
-        $(this)
-            .find("a")
-            .css({ background: "#2F2C2F", color: "#fff" })
-            .after(` <ul id="navPics" class="position_fixed margin_auto"></ul>`)
+    $ul.on("mouseenter", "li.tab", function () {
+        var a = $(this)
+            .find("a");
+        var data1 = a.attr("data1"),
+            data2 = a.attr("data2"),
+            data3 = a.attr("data3"),
+            data4 = a.attr("data4");
+        a.css({ background: "#2F2C2F", color: "#fff" })
+            .after(` <ul id="navPics" class="position_fixed margin_auto">
+            <li><a href="javascript:;"><img src="${data1}" alt="1"><div class="hoax"><div></a></li>
+            <li><a href="javascript:;"><img src="${data2}" alt="2"><div class="hoax"><div></a></li>
+            <li><a href="javascript:;"><img src="${data3}" alt="3"><div class="hoax"><div></a></li>
+            <li><a href="javascript:;"><img src="${data4}" alt="4"><div class="hoax"><div></a></li>
+        </ul>`)
             .next()
-            .removeClass("display_none")
             .animate({
-                top: 50     
-            }, 300)
+                top: 50
+            }, 300);
+            /*导航栏悬停内部的图片蒙层*/ 
+            $(".hoax").hover(
+                function(e){
+                $(e.target).animate({
+                    opacity:0.2,
+                    zIndex:-1
+                },200)
+            },
+            function(e){
+                $(e.target).animate({
+                    opacity:0
+                },200)
+            }
+            )
     })
-        .on("mouseleave", "li", function () {
+        .on("mouseleave", "li.tab", function () {
             $(this)
-            .find("a")
-            .css({background: "#fff",color: "#000"})
-                .children("a")
-                .css({ color: "#000" })
-           $("#navPics")
+                .find("a")
+                .css({ background: "#fff", color: "#000" });
+            $("#navPics")
                 .remove();
-        })
+        });
+
+
 })()
