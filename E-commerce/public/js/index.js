dd59778d8772b34e9c05ca3210e0e9af5b6cbd47
StellaYangF <1062878468@qq.com>
2019-03-04 21:14:52 +0800

    navs

--- E-commerce/public/js/index.js
+++ E-commerce/public/js/index.js
@@ -1,4 +1,4 @@
-(function() {
+(function () {
     $(".themeGreen")
         .css({ width: "1500px", position: "relative" })
         .children("ul")
@@ -23,39 +23,39 @@
         .css({ position: "relative" });
     // navs悬停设置
     var $ul = $("#header>ul");
-    $ul.on("mouseenter", "li.tab", function() {
-            var a = $(this)
-                .find("a");
-            var data1 = a.attr("data1"),
-                data2 = a.attr("data2"),
-                data3 = a.attr("data3"),
-                data4 = a.attr("data4");
-            a.css({ background: "#2F2C2F", color: "#fff" })
-                .after(` <ul id="navPics" class="position_fixed margin_auto">
+    $ul.on("mouseenter", "li.tab", function () {
+        var a = $(this)
+            .find("a");
+        var data1 = a.attr("data1"),
+            data2 = a.attr("data2"),
+            data3 = a.attr("data3"),
+            data4 = a.attr("data4");
+        a.css({ background: "#2F2C2F", color: "#fff" })
+            .after(` <ul id="navPics" class="fixed margin_auto">
             <li><a href="javascript:;"><div class="hoax"><img src="${data1}" alt="1"></div></a></li>
             <li><a href="javascript:;"><div class="hoax"><img src="${data2}" alt="2"></div></a></li>
             <li><a href="javascript:;"><div class="hoax"><img src="${data3}" alt="3"></div></a></li>
             <li><a href="javascript:;"><div class="hoax"><img src="${data4}" alt="4"></div></a></li>
         </ul>`)
-                .next()
-                .animate({
-                    top: 50
-                }, 300);
-            /*导航栏悬停内部的图片蒙层*/
-            $(".hoax").hover(
-                function(e) {
-                    $(e.target).animate({
-                        opacity: 0.7,
-                    }, 200)
-                },
-                function(e) {
-                    $(e.target).animate({
-                        opacity: 1
-                    }, 200)
-                }
-            )
-        })
-        .on("mouseleave", "li.tab", function() {
+            .next()
+            .animate({
+                top: 50
+            }, 300);
+        /*导航栏悬停内部的图片蒙层*/
+        $(".hoax").hover(
+            function (e) {
+                $(e.target).animate({
+                    opacity: 0.7,
+                }, 200)
+            },
+            function (e) {
+                $(e.target).animate({
+                    opacity: 1
+                }, 200)
+            }
+        )
+    })
+        .on("mouseleave", "li.tab", function () {
             $(this)
                 .find("a")
                 .css({ background: "#fff", color: "#000" });
@@ -66,14 +66,27 @@
     /*carousel*/
     var i = 0;
     var moved = -504.36;
-    var crs = setInterval(function() {
+    setInterval(function () {
         if (i < 11) {
             $(".carousel_img").css({
-                marginLeft: `${moved*i}px`
+                marginLeft: `${moved * i}px`
             });
             i++;
         } else {
             i = 0;
         }
     }, 3000)
+
+    /*navs fiexed*/
+    $header = $("#header");
+    var navOffset = $header.offset().top;
+    $(window).scroll(function () {
+        var scrollPos = $(window).scrollTop();
+        if (scrollPos >= navOffset) {
+            $header
+            .addClass("fixed")
+        } else {
+            $header.removeClass("fixed")
+        }
+    })
 })()
